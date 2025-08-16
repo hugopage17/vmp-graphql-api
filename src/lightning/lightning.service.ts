0bed4a9d5559a678fcb49911f14975795095b922
Hugo Page <hugopage0017@gmail.com>
2025-08-16 23:24:34 +1000

    fixed sort key

--- src/lightning/lightning.service.ts
+++ src/lightning/lightning.service.ts
@@ -13,7 +13,7 @@ export class LightningDataService {
     }
 
     public async listLightningStrikes(sortKey?: string) {
-        const lightningStrikes = await this.dbService.query<VolcanicLightningDBItem>("VOLCANIC_LIGHTNING", sortKey ?? "");
+        const lightningStrikes = await this.dbService.query<VolcanicLightningDBItem>("VOLCANIC_LIGHTNING", sortKey ?? "2");
         const strikeInfo = lightningStrikes?.[0]?.strikeInfo?.reduce((prev, curr) => {
             if (prev.find((p) => p.region === curr.region)) {
                 prev.find((p) => p.region === curr.region)?.info.push(curr)           
